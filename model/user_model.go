4290fcee0110e3cbef0d2b4ba777b99f397b5f4f
Alan <alancmp0304@gmail.com>
2022-04-27 18:07:20 +0800

    add database connection

--- model/user_model.go
+++ model/user_model.go
@@ -1,1 +1,20 @@
 package model
+
+import (
+	"go_blog/db"
+	"time"
+)
+
+type User struct {
+	ID        uint `gorm:"primaryKey"`
+	Username  string
+	Account   string `gorm:"uniqueIndex"`
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func Create(username, account, passwrod string) {
+	user := User{Username: username, Account: account, Password: passwrod}
+	result := db.DBm.Create(&user)
+}
